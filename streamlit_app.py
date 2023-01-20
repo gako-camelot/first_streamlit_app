c08ff98f0dec6e226392d37e97b5df2de8f01fc7
gako-camelot <119685886+gako-camelot@users.noreply.github.com>
2023-01-20 15:08:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+def get_fruity_vice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # streamlit.text(fruityvice_response.json())
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -31,12 +38,13 @@ try:
   else:
     # streamlit.write('The user entered ', fruit_choice)
 
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # streamlit.text(fruityvice_response.json())
-    # write your own comment -what does the next line do? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # write your own comment - what does this do?
-    streamlit.dataframe(fruityvice_normalized)
+#     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#     # streamlit.text(fruityvice_response.json())
+#     # write your own comment -what does the next line do? 
+#     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#     # write your own comment - what does this do?
+    back_from_function = get_fruity_vice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 
 except URLError as e:
   streamlit.error()
